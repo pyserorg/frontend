c2f1d8a41fba7399ba4352ebb87324177bb34458
Goran Mekić <meka@tilda.center>
2018-12-26 20:41:52 +0100

    Add blog

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -9,6 +9,8 @@ import {
 } from 'components/atoms/protected/reducers'
 
 // pages
+import { blogDetailReducer, blogDetailEditReducer } from 'pages/blog-detail/reducers'
+import { blogListReducer } from 'pages/blog-list/reducers'
 import galleryReducer from 'pages/gallery/reducers'
 import loginReducer from 'pages/login/reducers'
 
@@ -18,6 +20,9 @@ import titleReducer from 'templates/default/reducers'
 
 export default combineReducers({
   auth: authReducer,
+  blogDetail: blogDetailReducer,
+  blogDetailEdit: blogDetailEditReducer,
+  blogList: blogListReducer,
   error: errorReducer,
   gallery: galleryReducer,
   login: loginReducer,
--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,50 @@ export const getCookie = (name) => {
   if (parts.length === 2) return parts.pop().split(';').shift()
   return null
 }
+
+export const linkTarget = (url) => {
+  if (url.length < 4) {
+    return '_blank'
+  }
+  if (url[0] === '/') {
+    if (url[1] !== '/') {
+      return ''
+    }
+  }
+  return '_blank'
+}
+
+
+export const handleOver = (item, over, component) => () => {
+  if (over) {
+    component.setState({ over: item })
+  } else {
+    component.setState({ over: null })
+  }
+}
+
+
+export const handleEdit = (item, edit, component) => () => {
+  handleOver(item, false, component)()
+  if (edit) {
+    component.setState(prevState => ({
+      edit: item,
+      [`${item}Old`]: prevState[item],
+    }))
+  } else {
+    component.setState({ edit: null })
+  }
+}
+
+
+export const handleValue = (item, component, reset = false) => (event) => {
+  if (reset) {
+    component.setState(prevState => ({
+      [item]: prevState[`${item}Old`],
+      [`${item}Old`]: null,
+    }))
+    handleEdit(item, false, component)()
+  } else {
+    component.setState({ [item]: event.target.value })
+  }
+}
