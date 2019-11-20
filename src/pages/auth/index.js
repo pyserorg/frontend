857f51ac948e77903772993b01888d52e03d9de2
Goran Mekić <meka@tilda.center>
2019-11-20 17:45:58 +0100

    Reset password

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,12 +1,16 @@
+import changePassword from './change-password'
 import login from './login'
 import initial from './initial'
 import register from './register'
+import reset from './reset'
 import store from './store'
 
 
 export default {
+  changePassword,
   login,
   initial,
   register,
+  reset,
   store,
 }
